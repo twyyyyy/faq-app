f9cb0dbf38a86a7d152d20ef10aa5229d09c51c9
Mabel Teo <mabeltwy@gmail.com>
2026-02-15 21:22:17 +0800

    mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express"; // load express library (backend framework) into program
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express() // create a web server application instance 
-const port = 3000 // communication channel for backend. the computer can run many services -> ports separate them
+const port = process.env.PORT || 3000 // use environment-defined port, otherwise default. the computer can run many services -> ports separate them
+dotenv.config(); 
 
 // this portion means: when someone visits /, run this function. 
 // app.get() defines a route handler for HTTP GET requests 
@@ -13,3 +16,7 @@ app.get('/', (req,res) => {
 app.listen(port, () => { // start the server and listen for incoming requests on this port
     console.log(`Server running at http://localhost:${port}`)
 })
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
